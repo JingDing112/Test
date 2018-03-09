230d4e8527fee49f6120acd62e5031222cf2f242
Jing Ding <Jing.Ding@veritas.com>
2018-03-09 10:37:55 +0800

    init commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<10; i++){
+			System.out.println("Testing..."+i);
+		}
+		System.out.println("work done...");
+	}
+
+}
